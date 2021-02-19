a1b8554d5212911a5fde251b5a7490b7ee4e3ca2
Yeison Garzón T <toxiicsa@hotmail.com>
2021-02-19 02:04:50 -0500

    ADD: logic for resolution and imgs in a same field

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -1,14 +1,20 @@
 const _projectModel = require('../models/project.model');
 
- const get = async (id)=>{
+ const get = async (id, resolution)=>{
         if(!id){
             const error = new Error();
             error.status = 400;
             error.message = "id must be sent";
             throw error;
         }
+        if(!resolution){
+            const error = new Error();
+            error.status = 400;
+            error.message = "resolution must be sent";
+            throw error;
+        }
         
-        const currentProject = await _projectModel.findById(id);
+        const currentProject = await _projectModel.findById(id, `_id name description imgs_${resolution} finishDate technologies`);
 
         if(!currentProject){
             const error = new Error();
@@ -20,13 +26,44 @@ const _projectModel = require('../models/project.model');
         return currentProject;
     }
 
- const getAll = async (pageSize = 5, pageNum = 1)=>{
+ const getAll = async (resolution = 0, pageSize = 5, pageNum = 1, res)=>{
+
+        if(!resolution){
+            const error = new Error();
+            error.status = 400;
+            error.message = "resolution must be sent";
+            throw error;
+        }
+
         const skips = pageSize * (pageNum - 1);
-        return await _projectModel
-            .find()
-            .skip(skips)
-            .limit(pageSize);
-    }
+
+        let query = _projectModel
+        .find({}, `_id name description ${resolution} finishDate technologies`)
+        .skip(skips)
+        .limit(pageSize);
+        
+        query.exec(function (err, oldProject){
+            let newProjects = [];
+            let newProject = new Object;
+            
+            if(err){
+                return;
+            }
+            
+            JSON.stringify(oldProject);
+            for(let i= 0; i < oldProject.length; i++){
+                newProject['_id'] = oldProject[i]._id;
+                newProject['name'] = oldProject[i].name;
+                newProject['description'] = oldProject[i].description;
+                newProject['finishDate'] = oldProject[i].finishDate;
+                newProject['technologies'] = oldProject[i].technologies;
+                newProject['imgs'] = oldProject[i][resolution];
+                newProjects.push(newProject);
+            }
+
+            return res.send(newProjects);
+        });
+    }   
 
 module.exports = {
     get,
