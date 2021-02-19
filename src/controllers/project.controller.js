474c0a8a09b3c15904dda252feb1df792349e9f9
Yeison Garzón T <toxiicsa@hotmail.com>
2021-02-19 02:03:20 -0500

    ADD: resolution param

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -2,17 +2,16 @@
 
  const _projectService = require('../services/project.service');
     
-
     const get = async (req,res)=>{
-        const { projectId } = req.params;
-        const project = await _projectService.get(projectId);
+        const { projectId, resolution} = req.params;
+        const project = await _projectService.get(projectId, resolution);
         return res.send(project);
     }
 
     const getAll= async(req,res)=>{
+        const { resolution } = req.params;
         const { pageSize, pageNum } = req.query;
-        const projects = await _projectService.getAll(pageSize, pageNum);
-        return res.send(projects);
+        return await _projectService.getAll(resolution, pageSize, pageNum, res);
     }
 
    module.exports = {
